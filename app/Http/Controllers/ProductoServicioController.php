7456ab102ecad7c9ddc6fdecfb625c5d17f6344c
Kevin <kevinyahirt@gmail.com>
2025-10-08 17:36:18 -0600

    fea: consulta de registro de productos y servicios

--- app/Http/Controllers/ProductoServicioController.php
+++ app/Http/Controllers/ProductoServicioController.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\ProductoServicio;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class ProductoServicioController extends Controller
 {
-    //
+    public function index()
+    {
+        $productoServicios = ProductoServicio::all(); 
+
+        return Inertia::render('producto-servicios/index', [
+            'productoServicios' => $productoServicios 
+        ]);
+    }
 }
--- resources/js/pages/dashboard-healthcare.tsx
+++ resources/js/pages/dashboard-healthcare.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { FaUser } from "react-icons/fa";
+import { LuBedSingle } from "react-icons/lu";
+import { RiArchiveDrawerFill } from "react-icons/ri";
 import { route } from 'ziggy-js';
 import { Head, usePage, PageProps as InertiaPageProps, router } from '@inertiajs/react';
 import { User } from '@/types';
@@ -21,6 +23,8 @@ export default function Dashboard() {
             <Head title="Dashboard" />
             <MainLayout userName={user.nombre}>
                 <div className="p-4 max-w-sm mx-auto">
+                </div>
+                <div>
                     <CardButton
                         icon={FaUser}
                         text="Pacientes"
@@ -28,7 +32,22 @@ export default function Dashboard() {
                         onClick={() => router.visit(route('pacientes.index'))}
                     />
                 </div>
-                
+                <div>
+                    <CardButton
+                        icon={LuBedSingle}
+                        text="Habitaciones"
+                        bgColor="#1B1C38"
+                        onClick={() => router.visit(route('habitaciones.index'))}
+                    />
+                </div>
+                <div>
+                    <CardButton
+                        icon={RiArchiveDrawerFill}
+                        text="Productos y servicios"
+                        bgColor="#1B1C38"
+                        onClick={() => router.visit(route('producto-servicios.index'))}
+                    />
+                </div>
             </MainLayout>
         </>
     );
