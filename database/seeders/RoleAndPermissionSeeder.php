46603b6a3f5109b76f728f3b8da01aba56ac0f08
Kevin <kevinyahirt@gmail.com>
2025-10-07 20:14:32 -0600

    doc: modificacion diagrama relacional, se agrego campos de la tabla de servicios

--- database/seeders/RoleAndPermissionSeeder.php
+++ database/seeders/RoleAndPermissionSeeder.php
@@ -15,21 +15,38 @@ class RoleAndPermissionSeeder extends Seeder
      */
     public function run(): void
     {
-        $role = Role::create(['name' => 'admin']);
-        $role = Role::create(['name' => 'medico']);
-        $role = Role::create(['name' => 'medico_especialista']);
-        $role = Role::create(['name' => 'enfermera']);
-        $role = Role::create(['name' => 'administrativos']);
-        $role = Role::create(['name' => 'cocina']);
-        $role = Role::create(['name' => 'mantenimiento']);
-        $role = Role::create(['name' => 'farmacia']);
-        $role = Role::create(['name' => 'tecnico_radiologo']);
-        $role = Role::create(['name' => 'tecnico_laboratio']);
-        $role = Role::create(['name' => 'fisoterapeuta']);
+        //Roles
+        $roleAdmin = Role::create(['name' => 'admin']);
+        $roleMedico = Role::create(['name' => 'medico']);
+        $roleMedicoEspecialista = Role::create(['name' => 'medico_especialista']);
+        $roleEnfermera = Role::create(['name' => 'enfermera']);
+        $roleAdministrativos = Role::create(['name' => 'administrativos']);
+        $roleCocina = Role::create(['name' => 'cocina']);
+        $roleMantenimiento = Role::create(['name' => 'mantenimiento']);
+        $roleFarmacia = Role::create(['name' => 'farmacia']);
+        $roleTecnicoRadiologo = Role::create(['name' => 'tecnico_radiologo']);
+        $roleTecnicoLaboratorio = Role::create(['name' => 'tecnico_laboratorio']);
+        $roleFisoterapeuta = Role::create(['name' => 'fisoterapeuta']);
+        $roleRecepcion = Role::create(['name' => 'recepcion']);
 
-        $permission = Permission::create(['name' => 'edit articles']);
+        //Permisos
+        $permissions = [
+            'pacientes' => ['crear', 'consultar', 'editar', 'eliminar'],
+            'estancia' => ['crear', 'consultar', 'editar', 'eliminar'],
+            'hojafrontal' => ['crear', 'consultar', 'editar', 'eliminar'],
+            'habitacion' => ['crear', 'consultar', 'editar', 'eliminar'],
+        ];
+
+        foreach ($permissions as $module => $actions) {
+            foreach ($actions as $action) {
+                Permission::firstOrCreate([
+                    'name' => "{$action} {$module}",
+                ]);
+            }
+        }
+        
+
         
         
-        $role->givePermissionTo($permission);
     }
 }
