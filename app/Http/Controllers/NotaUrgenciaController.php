f3ac5eebb949d6e5a51ba30f813d20b993984685
EfrainHeGo <hgeo220247@upemor.edu.mx>
2025-11-10 12:08:10 -0600

    Nota de irgencias

--- app/Http/Controllers/NotaUrgenciaController.php
+++ app/Http/Controllers/NotaUrgenciaController.php
@@ -3,8 +3,67 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\NotaUrgencia;
+use App\Models\Paciente;
+use App\Models\Estancia;
+use App\Models\FormularioInstancia;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
+use Inertia\Inertia;
+use App\Http\Requests\NotaUrgenciaRequest;
 
 class NotaUrgenciaController extends Controller
 {
-    //
+    public function index()
+    {
+        //
+    }
+    public function create(Paciente $paciente, Estancia $estancia)
+    {
+        return Inertia::render('formularios/notaurgencia/create', [
+            'paciente' => $paciente,
+            'estancia' => $estancia,
+        ]);
+    }
+    public function store(Paciente $paciente, Estancia $estancia, NotaUrgenciaRequest $request)
+    {
+        $validatedData = $request->validate();
+        DB::beginTransaction();
+        try {
+            $formularioInstancia = FormularioInstancia::create([
+                'fecha_hora' => now(),  
+                'estancia_id' => $estancia->id,
+                'formulario_catalogo_id' => NotaUrgencia::CATALOGO_ID, 
+                'user_id' => Auth::id(),
+            ]);
+            $notaUrgencia = NotaUrgencia::create([
+                'id' => $formularioInstancia->id,
+                ...$validatedData
+            ]);
+            dd($notaUrgencia);
+            DB::commit();
+            return redirect()->route('estancias.show', [
+                'estancia' => $estancia->id,
+            ])->with('success', 'Nota de urgencia creada exitosamente.');
+        } catch (\Exception $e) {
+            DB::rollBack();
+            return redirect()->back()->withErrors(['error' => 'Error al crear la nota de urgencia: ' . $e->getMessage()])->withInput();
+        }
+    }
+    public function show()
+    {
+        //
+    }
+    public function edit()
+    {
+        //
+    }
+    public function update(Request $request)
+    {
+        //
+    }
+    public function destroy()
+    {
+        //
+    }
 }
--- app/Models/NotaUrgencia.php
+++ app/Models/NotaUrgencia.php
@@ -6,5 +6,29 @@ use Illuminate\Database\Eloquent\Model;
 
 class NotaUrgencia extends Model
 {
-    //
+    public const CATALOGO_ID = 9;
+    protected $table = 'nota_urgencia';
+    protected $fillable = [
+        'id',
+        'ta',
+        'fc',
+        'fr',
+        'temp',
+        'peso',
+        'talla',
+        'motivo_atencion',
+        'resumen_interrogatorio',
+        'exploracion_fisica',
+        'estado_mental',
+        'resultados_relevantes',
+        'diagnostico_problemas_clinicos',
+        'tratamiento',
+        'pronostico',
+        
+    ];
+    
+    public function formularioInstancia()
+    {
+        return $this->belongsTo(FormularioInstancia::class, 'id', 'id');
+    }
 }
