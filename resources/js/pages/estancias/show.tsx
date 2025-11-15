4461a011d6b60fafe84c72644366ad901bd3b107
Kevin <kevinyahirt@gmail.com>
2025-11-15 01:34:35 -0600

    fea: correcion de la nota postoperatoria en pdf

--- resources/js/pages/estancias/show.tsx
+++ resources/js/pages/estancias/show.tsx
@@ -163,6 +163,7 @@ const Show = ({ estancia }: ShowEstanciaProps) => {
                                     <Menu.Item>
                                         {({ active }) => (
                                             <Link
+                                                href={route('pacientes.estancias.notaspostoperatorias.create', { paciente: paciente.id, estancia: estancia.id })}
                                                  className={`${
                                                     active ? 'bg-blue-500 text-white' : 'text-gray-900'
                                                 } group flex rounded-md items-center w-full px-2 py-2 text-sm`}
