60d22c9b9907de4fc152464dbe93893f8af58b36
Kevin Yahir <kevinyahirt@gmail.com>
2026-01-05 15:48:09 -0600

    fea: envio de solicitud de dieta y visualizacion en el pdf

--- app/Models/SolicitudDieta.php
+++ app/Models/SolicitudDieta.php
@@ -3,27 +3,40 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class SolicitudDieta extends Model
 {
     protected $fillable = [
         'id',
         'hoja_enfermeria_id',
-        'tipo_dieta',
-        'opcion_seleccionada',
+
+        'dieta_id',
+
         'horario_solicitud',
         'user_supervisa_id',
+        
         'horario_entrega',
         'user_entrega_id',
-        'horario_operacion',
-        'horario_termino',
-        'horario_inicio_dieta',
+        
+        'observaciones'
+        
     ];
 
     public $timestamps = false;
 
-    public function usuarioSupervisa()
+    public function usuarioSupervisa():BelongsTo
     {
-        return $this->belongsTo(User::class, 'user_supervisa_id');
+        return $this->belongsTo(User::class, 'user_supervisa_id', 'id');
+    }
+
+    public function userEntrega():BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_entrega_id', 'id');
+    }
+
+    public function dieta():BelongsTo
+    {
+        return $this->belongsTo(Dieta::class);
     }
 }
