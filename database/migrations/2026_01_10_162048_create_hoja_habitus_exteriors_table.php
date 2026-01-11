c41c6c1caad3934a4a5cf9477690694e4477f9b3
Kevin Yahir <kevinyahirt@gmail.com>
2026-01-11 16:02:34 -0600

    fea: creacion de componentes para la gestion de habitus exterior

---
+++ database/migrations/2026_01_10_162048_create_hoja_habitus_exteriors_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('hoja_habitus_exteriors', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('hoja_enfermeria_id')
+                ->constrained('hoja_enfermerias')
+                ->onDelete('cascade');
+
+            //$table->string();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('hoja_habitus_exteriors');
+    }
+};
