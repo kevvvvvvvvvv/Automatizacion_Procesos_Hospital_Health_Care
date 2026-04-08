9b54b6a2ccc4a0b7c61127c3b6f2d0b714a2d1e2
Kevin <kevinyahirt@gmail.com>
2026-04-08 08:33:25 -0600

    chore: cambios

--- app/Http/Controllers/Caja/ContaduriaController.php
+++ app/Http/Controllers/Caja/ContaduriaController.php
@@ -23,6 +23,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\DB;
 
+
 class ContaduriaController extends Controller
 {
     public function index()
