bfab1b3ce415f7fbc658a8d347cd8a61ef226461
user.name <kevinyahirt@gmail.com>
2026-04-12 11:06:27 -0600

    fix: se corrigio la vista del show de la reservacion de quirofano

--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -1100,7 +1100,7 @@ export interface ReservacionQuirofano {
     user?: User;
     habitacion?: Habitacion;
     medico_tratante?: User; 
-    medico_operacion?: User; 
+    medico_operacion_rel?: User; 
 }
 
 export interface Cargo {
